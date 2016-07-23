3c1124ae6ea206a0bffac3929cbf1127315ec804
Gautam Dey <gautam.dey77@gmail.com>
2016-07-23 12:10:31 -0700

    [wip]

---
+++ mvt/provider/provider.go
@@ -0,0 +1,48 @@
+package provider
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/terranodo/tegola/mvt"
+)
+
+// InitFunc initilize a provider given a config map. The init function should validate the config map, and report any errors. This is called by the For function.
+type InitFunc func(map[string]interface{}) (mvt.Provider, error)
+
+var providers map[string]InitFunc
+
+// Register is called by the init functions of the provider.
+func Register(name string, init InitFunc) error {
+	if providers == nil {
+		providers = make(map[string]InitFunc)
+	}
+	if _, ok := providers[name]; ok {
+		return fmt.Errorf("Provider %v already exists", name)
+	}
+	providers[name] = init
+	return nil
+}
+
+// Drivers returns a list of drivers that have registered.
+func Drivers() (l []string) {
+	if providers == nil {
+		return l
+	}
+	for k, _ := range providers {
+		l = append(l, k)
+	}
+	return l
+}
+
+// For function returns a configed provider of the given type, provided the correct config map.
+func For(name string, config map[string]interface{}) (mvt.Provider, error) {
+	if providers == nil {
+		return nil, fmt.Errorf("No providers registered.")
+	}
+	p, ok := providers[name]
+	if !ok {
+		return nil, fmt.Errorf("No providers registered by the name: %v, known providers(%v)", name, strings.Join(Drivers(), ","))
+	}
+	return p(config)
+}
--- provider/postgis/postgis.go
+++ provider/postgis/postgis.go
@@ -1,23 +1,29 @@
 package postgis
 
 import (
-	"bytes"
-	"fmt"
-	"strings"
 	"text/template"
 
 	"github.com/jackc/pgx"
 
-	"github.com/terranodo/tegola"
 	"github.com/terranodo/tegola/mvt"
-	"github.com/terranodo/tegola/wkb"
+	"github.com/terranodo/tegola/mvt/provider"
 )
+
+// Query holds information about a query.
+type Query struct {
+	// The SQL to use. !BBOX! token will be replaced by the envelope
+	SQL *template.Template
+	// The ID field name, this will default to 'gid' if not set to something other then empty string.
+	IDFieldname string
+	// The Geometery field name, this will default to 'geom' if not set to soemthing other then empty string.
+	GeomFieldName string
+}
 
 // Provider provides the postgis data provider.
 type Provider struct {
 	config pgx.ConnPoolConfig
 	pool   *pgx.ConnPool
-	layers map[string]*template.Template // map of layer name and corrosponding sql
+	layers map[string]Query // map of layer name and corrosponding sql
 	srid   int
 }
 
@@ -29,27 +35,118 @@ type Config struct {
 	User           string
 	Password       string
 	MaxConnections *uint8 // Default is 5 if nil, 0 means no max.
-	Layers         map[string]string
-	SRID           *int // Defaults to 3857
+	// Each layer should have a name a query that describes how to get the Features for that layer.
+	Layers map[string]Query
+	SRID   *int // Defaults to 3857
 }
 
 // DEFAULT sql for get geometeries,
 const stdSQL = `
-SELECT
-	ST_AsBinary(geom) AS geom,
-	zone_name AS "name",
-	gid
+SELECT *
 FROM
 	%[1]v
 WHERE
-	geom && {{.BBox}}
+	%[2]v && {{.BBox}}
 `
 
-// NewProvider Setups and returns a new postgis provider that can be used to get
+const Name = "postgis"
+
+func init() {
+	provider.Register(Name, NewProvider)
+}
+
+type confType map[string]interface{}
+
+func (c confType) getString(key string)(v string,err error){
+
+	var val interface{}
+	var ok bool
+	if val, ok =  config[key]; !ok {
+		return v, fmt.Errorf("%v key missing in config for %v provider.",key,Name)
+	}
+	if v, ok = val.(string); !ok {
+		return v, fmt.Errorf("%v value needs to be a string.",key)
+	}
+	return v, nil
+}
+func (c confType) getuInt16(key string)(v uint16,err error){
+	var val interface{}
+	var ok bool
+	if val, ok =  config[key]; !ok {
+		return v, fmt.Errorf("%v key missing in config for %v provider.",key,Name)
+	}
+	if v, ok = val.(uint16); !ok {
+		return v, fmt.Errorf("%v value needs to be an uint16 value.",key)
+	}
+	return v, nil
+}
+func (c confType) getPInt8(key string,def int8)(v int8,err error){
+	var val interface{}
+	var ok bool
+	if val, ok =  config[key]; !ok {
+		return v, fmt.Errorf("%v key missing in config for %v provider.",key,Name)
+	}
+	if v, ok = val.(*int8); !ok {
+		return v, fmt.Errorf("%v value needs to be an uint16 value.",key)
+	}
+	if v == nil {
+		return def, nil
+	}
+	return *v, nil
+}
+
+// NewProvider Setups and returns a new postgis provide or an error; if something
+// is wrong. The function will validate that the config object looks good before
+// trying to create a driver. This means that the Provider expects the following
+// fields to exists in the provided map[string]interface{} map.
+// Host string — the host to connect to.
+// Port uint16 — the port to connect on.
+// Database string — the database name
+// User string — the user name
+// Password string — the Password
+// MaxConnections *uint8 // Default is 5 if nil, 0 means no max.
+// Queries map[string]struct{ — This is map of layers keyed by the layer name.
+//     TableName string || SQL string — This is the sql to use or the tablename to use with the default query.
+//     Fields []string — This is a list, if this is nil or empty we will get all fields.
+//     GeometryFieldname string — This is the field name of the geometry, if it's an empty string or nil, it will defaults to 'geom'.
+//     IDFieldname string — This is the field name for the id property, if it's an empty string or nil, it will defaults to 'gid'.
+//  }
+func NewProvider(config map[string]interface{}) (mvt.Provider, error) {
+	// Validate the config to make sure it has the values I care about and the types for those values.
+	c := confType(config)
+	host, err := c.getString("host")
+	if err != nil {
+		return nil, err
+	}
+	port, err := c.getuInt16("port")
+	if err != nil {
+		return nil, err
+	}
+	db, err := c.getString("db")
+	if err != nil {
+		return nil, err
+	}
+	user, err := c.getString("user")
+	if err != nil {
+		return nil, err
+	}
+	password, err := c.getString("password")
+	if err != nil {
+		return nil, err
+	}
+	mcon, err := c.
+
+
+
+
+}
+
+// NewProviderOld Setups and returns a new postgis provider that can be used to get
 // tiles for layers.
 // name is the name for this Provider.
 // layers is a map of the layer name to the sql to run on postgis
-func NewProvider(config Config) (*Provider, error) {
+/*
+func NewProviderOld(config Config) (*Provider, error) {
 
 	conf := pgx.ConnConfig{
 		Host:     config.Host,
@@ -82,11 +179,12 @@ func NewProvider(config Config) (*Provider, error) {
 		layers: map[string]*template.Template{},
 	}
 
-	for name, tplStr := range config.Layers {
+	for name, query := range config.Layers {
 		tpl := template.New(name)
+		if query.SQL == ""
 		//	check for template
-		if !(strings.Contains(tplStr, "{{") && strings.Contains(tplStr, "}}")) {
-			tplStr = fmt.Sprintf(stdSQL, tplStr)
+		if !strings.Contains(tplStr, "!BBOX!") {
+			tplStr = fmt.Sprintf(stdSQL, tplStr, "geom", "gid", ",name")
 		}
 		if _, err := tpl.Parse(tplStr); err != nil {
 			return nil, fmt.Errorf("Layer %v template( %v ) had an error: %v", name, tplStr, err)
@@ -95,9 +193,10 @@ func NewProvider(config Config) (*Provider, error) {
 	}
 	return &p, nil
 }
-
+*/
 // MVTLayer returns a mvt.Layer
-func (p *Provider) MVTLayer(layerName string, tile tegola.Tile) (layer *mvt.Layer, err error) {
+/*
+func (p *Provider) MVTLayer(layerName string, tile tegola.Tile, tags map[string]interface{}) (layer *mvt.Layer, err error) {
 	if p == nil {
 		return nil, fmt.Errorf("Provider is nil")
 	}
@@ -143,9 +242,9 @@ func (p *Provider) MVTLayer(layerName string, tile tegola.Tile) (layer *mvt.Laye
 
 		var gid uint64
 		//	tags
-		gtags := map[string]interface{}{
-			"class": "park",
-		}
+			gtags := map[string]interface{}{
+				"class": "park",
+			}
 
 		//	scan data returned from database
 		if err = rows.Scan(&rgeom, &gname, &gid); err != nil {
@@ -179,3 +278,4 @@ func (p *Provider) MVTLayer(layerName string, tile tegola.Tile) (layer *mvt.Laye
 
 	return layer, nil
 }
+*/
