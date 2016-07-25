5d960a5b0cef49c9442d0efb3e0b2932f1b6f83e
Gautam Dey <gautam.dey77@gmail.com>
2016-07-25 07:27:38 +0000

    [wip] got the main code done.
    Got the main bit of logic done. Still have to rewrite the test, as it is still using the old style.

---
+++ Util/dict/gen.pl
@@ -0,0 +1,64 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use 5.10.0;
+
+my @types = qw(string int uint);
+push @types, "int$_", "uint$_" for (qw(8 16 32 64));
+
+say <<GOCODE;
+/* This file was generated using gen.pl and go fmt. */
+package dict
+
+type M map[string]interface{}
+
+func (m M)Dict(key string)(v M, err error){
+    var val interface{}
+    var ok bool
+    if val, ok = config[key]; !ok {
+        return v, fmt.Errorf("%v value is required.",key)
+    }
+    if mv, ok = val.(map[string]interface{}); !ok {
+        return v, fmt.Errorf("%v value needs to be of type map[string]interface{}.", key)
+    }
+    return M(mv), nil
+}
+
+GOCODE
+
+for my $T ( @types ) {
+    my $fnName = ucfirst($T);
+    my $fnSliceName = ucfirst($T).'Slice';
+    my $T_ptr = '*'.$T;
+    say <<GOCODE;
+    // $fnName returns the value as a $T type, if it is unable to convert the value it will error. If the default value is not provided, and it can not find the value, it will return the zero value, and an error.
+func (m M) $fnName(key string, def $T_ptr)(v $T, err error){
+    var val interface{}
+    var ok bool
+    if val, ok = config[key]; !ok {
+        if def != nil {
+            return \*def, nil
+        }
+        return v, fmt.Errorf("%v value is required.",key)
+    }
+    if v, ok = val.($T); !ok {
+        return \*def, fmt.Errorf("%v value needs to be of type ${T}.", key)
+    }
+    return v, nil
+}
+
+func (m M)$fnSliceName(key string)(v []$T, err error){
+    var val interface{}
+    var ok bool
+    if val, ok = config[key]; !ok {
+        return v,nil
+    }
+    if v, ok = val.([]$T); !ok {
+        return v, fmt.Errorf("%v value needs to be of type []${T}.", key)
+    }
+    return v, nil
+}
+
+GOCODE
+}
+
--- provider/postgis/postgis.go
+++ provider/postgis/postgis.go
@@ -1,18 +1,23 @@
 package postgis
 
 import (
-	"text/template"
+	"fmt"
+	"log"
+	"strings"
 
 	"github.com/jackc/pgx"
 
+	"github.com/terranodo/tegola"
+	"github.com/terranodo/tegola/Util/dict"
 	"github.com/terranodo/tegola/mvt"
 	"github.com/terranodo/tegola/mvt/provider"
+	"github.com/terranodo/tegola/wkb"
 )
 
-// Query holds information about a query.
-type Query struct {
+// layer holds information about a query.
+type layer struct {
 	// The SQL to use. !BBOX! token will be replaced by the envelope
-	SQL *template.Template
+	SQL string
 	// The ID field name, this will default to 'gid' if not set to something other then empty string.
 	IDFieldname string
 	// The Geometery field name, this will default to 'geom' if not set to soemthing other then empty string.
@@ -23,259 +28,274 @@ type Query struct {
 type Provider struct {
 	config pgx.ConnPoolConfig
 	pool   *pgx.ConnPool
-	layers map[string]Query // map of layer name and corrosponding sql
+	layers map[string]layer // map of layer name and corrosponding sql
 	srid   int
 }
 
-// Config is the main config structure for configuring this Provider.
-type Config struct {
-	Host           string
-	Port           uint16
-	Database       string
-	User           string
-	Password       string
-	MaxConnections *uint8 // Default is 5 if nil, 0 means no max.
-	// Each layer should have a name a query that describes how to get the Features for that layer.
-	Layers map[string]Query
-	SRID   *int // Defaults to 3857
-}
-
-// DEFAULT sql for get geometeries,
+// DEFAULT sql for get geometries,
+const BBOX = "!BBOX!"
 const stdSQL = `
-SELECT *
+SELECT %[1]v
 FROM
-	%[1]v
+	%[2]v
 WHERE
-	%[2]v && {{.BBox}}
-`
+	%[3]v && ` + BBOX
 
 const Name = "postgis"
+const DefaultPort = 5432
+const DefaultSRID = 3857
+const DefaultMaxConn = 5
+
+const (
+	ConfigKeyHost        = "host"
+	ConfigKeyPort        = "port"
+	ConfigKeyDB          = "db"
+	ConfigKeyUser        = "user"
+	ConfigKeyPassword    = "password"
+	ConfigKeyMaxConn     = "max_connection"
+	ConfigKeySRID        = "srid"
+	ConfigKeyLayers      = "layers"
+	ConfigKeyTablename   = "tablename"
+	ConfigKeySQL         = "sql"
+	ConfigKeyFields      = "fields"
+	ConfigKeyGeomField   = "geometry_fieldname"
+	ConfigKeyGeomIDField = "id_fieldname"
+)
 
 func init() {
 	provider.Register(Name, NewProvider)
-}
-
-type confType map[string]interface{}
-
-func (c confType) getString(key string)(v string,err error){
-
-	var val interface{}
-	var ok bool
-	if val, ok =  config[key]; !ok {
-		return v, fmt.Errorf("%v key missing in config for %v provider.",key,Name)
-	}
-	if v, ok = val.(string); !ok {
-		return v, fmt.Errorf("%v value needs to be a string.",key)
-	}
-	return v, nil
-}
-func (c confType) getuInt16(key string)(v uint16,err error){
-	var val interface{}
-	var ok bool
-	if val, ok =  config[key]; !ok {
-		return v, fmt.Errorf("%v key missing in config for %v provider.",key,Name)
-	}
-	if v, ok = val.(uint16); !ok {
-		return v, fmt.Errorf("%v value needs to be an uint16 value.",key)
-	}
-	return v, nil
-}
-func (c confType) getPInt8(key string,def int8)(v int8,err error){
-	var val interface{}
-	var ok bool
-	if val, ok =  config[key]; !ok {
-		return v, fmt.Errorf("%v key missing in config for %v provider.",key,Name)
-	}
-	if v, ok = val.(*int8); !ok {
-		return v, fmt.Errorf("%v value needs to be an uint16 value.",key)
-	}
-	if v == nil {
-		return def, nil
-	}
-	return *v, nil
 }
 
 // NewProvider Setups and returns a new postgis provide or an error; if something
 // is wrong. The function will validate that the config object looks good before
 // trying to create a driver. This means that the Provider expects the following
 // fields to exists in the provided map[string]interface{} map.
-// Host string — the host to connect to.
-// Port uint16 — the port to connect on.
-// Database string — the database name
-// User string — the user name
-// Password string — the Password
-// MaxConnections *uint8 // Default is 5 if nil, 0 means no max.
-// Queries map[string]struct{ — This is map of layers keyed by the layer name.
-//     TableName string || SQL string — This is the sql to use or the tablename to use with the default query.
-//     Fields []string — This is a list, if this is nil or empty we will get all fields.
-//     GeometryFieldname string — This is the field name of the geometry, if it's an empty string or nil, it will defaults to 'geom'.
-//     IDFieldname string — This is the field name for the id property, if it's an empty string or nil, it will defaults to 'gid'.
+// host string — the host to connect to.
+// port uint16 — the port to connect on.
+// database string — the database name
+// user string — the user name
+// password string — the Password
+// max_connections *uint8 // Default is 5 if nil, 0 means no max.
+// layers map[string]struct{ — This is map of layers keyed by the layer name.
+//     tablename string || sql string — This is the sql to use or the tablename to use with the default query.
+//     fields []string — This is a list, if this is nil or empty we will get all fields.
+//     geometry_fieldname string — This is the field name of the geometry, if it's an empty string or nil, it will defaults to 'geom'.
+//     id_fieldname string — This is the field name for the id property, if it's an empty string or nil, it will defaults to 'gid'.
 //  }
 func NewProvider(config map[string]interface{}) (mvt.Provider, error) {
 	// Validate the config to make sure it has the values I care about and the types for those values.
-	c := confType(config)
-	host, err := c.getString("host")
-	if err != nil {
-		return nil, err
-	}
-	port, err := c.getuInt16("port")
-	if err != nil {
-		return nil, err
-	}
-	db, err := c.getString("db")
-	if err != nil {
-		return nil, err
-	}
-	user, err := c.getString("user")
-	if err != nil {
-		return nil, err
-	}
-	password, err := c.getString("password")
-	if err != nil {
-		return nil, err
-	}
-	mcon, err := c.
+	c := dict.M(config)
 
-
-
-
-}
-
-// NewProviderOld Setups and returns a new postgis provider that can be used to get
-// tiles for layers.
-// name is the name for this Provider.
-// layers is a map of the layer name to the sql to run on postgis
-/*
-func NewProviderOld(config Config) (*Provider, error) {
-
-	conf := pgx.ConnConfig{
-		Host:     config.Host,
-		Port:     config.Port,
-		Database: config.Database,
-		User:     config.User,
-		Password: config.Password,
-	}
-	srid := 3857
-	if config.SRID != nil {
-		srid = *config.SRID
-	}
-	mconn := 5
-	if config.MaxConnections != nil {
-		mconn = int(*config.MaxConnections)
-	}
-	poolConfig := pgx.ConnPoolConfig{
-		MaxConnections: mconn,
-		ConnConfig:     conf,
-	}
-	connPool, err := pgx.NewConnPool(poolConfig)
+	host, err := c.String(ConfigKeyHost, nil)
 	if err != nil {
 		return nil, err
 	}
 
+	db, err := c.String(ConfigKeyDB, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	user, err := c.String(ConfigKeyUser, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	password, err := c.String(ConfigKeyPassword, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	port := int16(DefaultPort)
+	if port, err = c.Int16(ConfigKeyPort, &port); err != nil {
+		return nil, err
+	}
+
+	maxcon := int8(DefaultMaxConn)
+	if maxcon, err = c.Int8(ConfigKeyMaxConn, &maxcon); err != nil {
+		return nil, err
+	}
+
+	var srid = int(DefaultSRID)
+	if srid, err = c.Int(ConfigKeySRID, &srid); err != nil {
+		return nil, err
+	}
+
+	layers, ok := c[ConfigKeyLayers].(map[string]map[string]interface{})
+	if !ok {
+		return nil, fmt.Errorf("Expected %v to be a map[string]map[string]interface{}", ConfigKeyLayers)
+	}
+
+	lyrs := make(map[string]layer)
+	zerostr := ""
+
+	for lname, v := range layers {
+		vc := dict.M(v)
+		tblName, err := vc.String(ConfigKeyTablename, &zerostr)
+		if err != nil {
+			return nil, fmt.Errorf("for %v layer %v has an error: %v", lname, ConfigKeyTablename, err)
+		}
+		sql, err := vc.String(ConfigKeySQL, &zerostr)
+		if err != nil {
+			return nil, fmt.Errorf("for %v layer %v has an error: %v", lname, ConfigKeySQL, err)
+		}
+		if tablename == "" && sql == "" {
+			return nil, fmt.Errorf("The %v or %v field for layer %v must be specified.", ConfigKeyTablename, ConfigKeySQLlname, lname)
+		}
+		if tablename != "" && sql != "" {
+			log.Printf("Both %v and %v field are specified for layer %v, using only %[2]v field.", ConfigKeyTablename, ConfigKeySQL, lname)
+		}
+
+		fields, err := vm.StringSlice(ConfigKeyFields)
+		if err != nil {
+			return nil, fmt.Errorf("For layer %v %v field had the following error: %v", lname, ConfigKeyFields, err)
+		}
+		fld := "geom"
+		geomfld, err := vm.String(ConfigKeyGeomField, &fld)
+		if err != nil {
+			return nil, fmt.Errorf("For layer %v : %v", lname, err)
+		}
+		fld = "gid"
+		idfld, err := vm.String(ConfigKeyGeomIDField, &fld)
+		if err != nil {
+			return nil, fmt.Errorf("For layer %v : %v", lname, err)
+		}
+		if idfld == geomfld {
+			return nil, fmt.Errorf("For layer %v: %v (%v) and %v field (%v) is the same!", lname, ConfigKeyGeomField, geomfld, ConfigKeyGeomIDField, idfld)
+		}
+		var lsql string
+		if sql != "" {
+			// We need to make sure that the sql has a BBOX for the bounding box env.
+			if !strings.Contains(sql, BBOX) {
+				return nil, fmt.Errorf("SQL for layer %v does not contain "+BBOX+", entry.", lname)
+			}
+			if !strings.Contains(sql, "*") {
+				if !strings.Contains(sql, geomfld) {
+					return nil, fmt.Errorf("SQL for layer %v does not contain the geometry field: %v", lname, geomfld)
+				}
+				if !strings.Contains(sql, idfld) {
+					return nil, fmt.Errorf("SQL for layer %v does not contain the id field for the geometry: %v", lname, idfld)
+				}
+			}
+			lsql = sql
+		} else {
+			// Tablename and Fields will be used to
+			// We need to do some work. We need to check to see Fields contains the geom and gid fields
+			// and if not add them to the list. If Fields list is empty/nil we will use '*' for the field
+			// list.
+			selectClause := "*"
+			if len(fields) != 0 {
+				var fgeom, fgid bool
+				for _, f := range fields {
+					if f == geomfld {
+						fgeom = true
+					}
+					if f == idfld {
+						idfld = true
+					}
+				}
+				if !fgeom {
+					fields = append(fields, geomfld)
+				}
+				if !idfld {
+					fields = append(fields, idfld)
+				}
+				selectClause = strings.Join(fields, ",")
+			}
+			lsql = fmt.Sprintf(stdSQL, selectClause, tablename, geomfld)
+		}
+		lyrs[lname] = layer{
+			SQL:           lsql,
+			IDFieldname:   idfld,
+			GeomFieldName: geomfld,
+		}
+	}
 	p := Provider{
-		config: poolConfig,
-		pool:   connPool,
 		srid:   srid,
-		layers: map[string]*template.Template{},
+		layers: lyrs,
+		config: pgx.ConnPoolConfig{
+			ConnConfig: pgx.Config{
+				Host:     host,
+				Port:     port,
+				Database: db,
+				User:     user,
+				Password: password,
+			},
+			MaxConnections: maxcon,
+		},
+	}
+	if p.connPool, err = pgx.NewConnPool(p.config); err != nil {
+		return nil, fmt.Errorf("Failed while creating connection pool: %v", err)
 	}
 
-	for name, query := range config.Layers {
-		tpl := template.New(name)
-		if query.SQL == ""
-		//	check for template
-		if !strings.Contains(tplStr, "!BBOX!") {
-			tplStr = fmt.Sprintf(stdSQL, tplStr, "geom", "gid", ",name")
-		}
-		if _, err := tpl.Parse(tplStr); err != nil {
-			return nil, fmt.Errorf("Layer %v template( %v ) had an error: %v", name, tplStr, err)
-		}
-		p.layers[name] = tpl
-	}
-	return &p, nil
+	return p, nil
 }
-*/
-// MVTLayer returns a mvt.Layer
-/*
-func (p *Provider) MVTLayer(layerName string, tile tegola.Tile, tags map[string]interface{}) (layer *mvt.Layer, err error) {
-	if p == nil {
-		return nil, fmt.Errorf("Provider is nil")
+
+func (p Provider) LayerNames() (names []string) {
+	for k, _ := range p.layers {
+		names = append(names, k)
 	}
+	return names
+}
 
-	extent := tile.Extent()
-
-	//	build out our template bbox template
-	tpl := struct {
-		Name string
-		BBox string
-	}{
-		Name: layerName,
-		BBox: fmt.Sprintf("ST_MakeEnvelope(%v,%v,%v,%v,%v)", extent.Minx, extent.Miny, extent.Maxx, extent.Maxy, p.srid),
-	}
-
-	var sr bytes.Buffer
-
-	t, ok := p.layers[layerName]
+func (p Provider) MVTLayer(layerName string, tile tegola.Tile, tags map[string]interface{}) (layer *mvt.Layer, err error) {
+	textent := tile.Extent()
+	bbox := fmt.Sprintf("ST_MakeEnvelope(%v,%v,%v,%v,%v)", textent.Minx, textent.Miny, textent.Maxx, textent.Maxy, p.srid)
+	plyr, ok := p.layers[layerName]
 	if !ok {
 		return nil, fmt.Errorf("Don't know of the layer %v", layerName)
 	}
-	//	execute our template
-	t.Execute(&sr, tpl)
+	sql := strings.Replace(plyr.SQL, BBOX, bbox, -1)
 
-	sql := sr.String()
+	layer = new(mvt.Layer)
+	layer.Name = layerName
 
-	//	log.Println("sql", sql)
-
-	//	execute query
 	rows, err := p.pool.Query(sql)
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 
-	layer = new(mvt.Layer)
-	layer.Name = layerName
+	fdescs := rows.FieldDescriptions()
+	var geobytes []byte
 
-	// Iterate through the result set
 	for rows.Next() {
-		var rgeom []byte
-		gname := new(*string)
-
+		var geom tegola.Geometry
 		var gid uint64
-		//	tags
-			gtags := map[string]interface{}{
-				"class": "park",
+		vals, err := rows.Values()
+		if err != nil {
+			return nil, fmt.Errorf("Got an error trying to run SQL: %v ; %v", sql, err)
+		}
+		gtags := make(map[string]interface{})
+		for i, v := range vals {
+			switch fdecs[i].Name {
+			case plyr.GeomFieldName:
+				if geobytes, ok = v.([]byte); !ok {
+					return nil, fmt.Errorf("Was unable to convert geometry field(%v) into bytes for layer %v.", plyr.GeomFieldName, layerName)
+				}
+				if geom, err = wkb.DecodeBytes(geobytes); err != nil {
+					return nil, fmt.Errorf("Was unable to decode geometry field(%v) into wkb for layer %v.", plyr.GeomFieldName, layerName)
+				}
+			case plyr.IDFieldName:
+				if gid, ok = v.(uint64); !ok {
+					return nil, fmt.Errorf("Unable to convert geometry ID field(%v) into a uint64 for layer %v", fdecs[i].IDFieldname, layerName)
+				}
+			default:
+				gtags[fdecs[i].Name] = vals[i]
 			}
-
-		//	scan data returned from database
-		if err = rows.Scan(&rgeom, &gname, &gid); err != nil {
-			return nil, err
 		}
-
-		//	gecode our geometry
-		geom, err := wkb.DecodeBytes(rgeom)
-		if err != nil {
-			return nil, err
+		for k, v := range tags {
+			// If tags does not exists, then let's add it.
+			if _, ok = gtags[k]; !ok {
+				gtags[k] = v
+			}
 		}
-		//		log.Printf("Got geo: %v", wkb.WKT(geom))
-
-		//	TODO: Need to support collection geometries.
-		if _, ok := geom.(tegola.Collection); ok {
-			return nil, fmt.Errorf("For Layer (%v) and geometry name(%v); Geometry collections are not supported.", layerName, gname)
-		}
-
-		if err != nil {
-			geostr := wkb.WKT(geom)
-			return nil, fmt.Errorf("Error trying to rehome %v : %v", geostr, err)
-		}
-
-		//	add features to layer
+		// Add features to Layer
 		layer.AddFeatures(mvt.Feature{
 			ID:       &gid,
 			Tags:     gtags,
 			Geometry: geom,
 		})
 	}
-
-	return layer, nil
+	return layer, err
 }
-*/
