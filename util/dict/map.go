8f2d0b4fbfa64d668da88b65e9c17ef62bc0fd35
Gautam Dey <gautam.dey77@gmail.com>
2016-07-26 00:21:45 +0000

    [wip] Cleaned up postgis_test

--- util/dict/map.go
+++ util/dict/map.go
@@ -1,16 +1,20 @@
 /* This file was generated using gen.pl and go fmt. */
+// dict is a helper function that allow one to easily get concreate values out of a map[string]interface{}
 package dict
 
 import "fmt"
 
 type M map[string]interface{}
 
+// Dict is to obtain a map[string]interface{} that has already been cast to a M type.
 func (m M) Dict(key string) (v M, err error) {
 	var val interface{}
+	var mv map[string]interface{}
 	var ok bool
-	if val, ok = config[key]; !ok {
+	if val, ok = m[key]; !ok {
 		return v, fmt.Errorf("%v value is required.", key)
 	}
+
 	if mv, ok = val.(map[string]interface{}); !ok {
 		return v, fmt.Errorf("%v value needs to be of type map[string]interface{}.", key)
 	}
@@ -21,7 +25,7 @@ func (m M) Dict(key string) (v M, err error) {
 func (m M) String(key string, def *string) (v string, err error) {
 	var val interface{}
 	var ok bool
-	if val, ok = config[key]; !ok {
+	if val, ok = m[key]; !ok {
 		if def != nil {
 			return *def, nil
 		}
@@ -36,7 +40,7 @@ func (m M) String(key string, def *string) (v string, err error) {
 func (m M) StringSlice(key string) (v []string, err error) {
 	var val interface{}
 	var ok bool
-	if val, ok = config[key]; !ok {
+	if val, ok = m[key]; !ok {
 		return v, nil
 	}
 	if v, ok = val.([]string); !ok {
@@ -49,7 +53,7 @@ func (m M) StringSlice(key string) (v []string, err error) {
 func (m M) Int(key string, def *int) (v int, err error) {
 	var val interface{}
 	var ok bool
-	if val, ok = config[key]; !ok {
+	if val, ok = m[key]; !ok {
 		if def != nil {
 			return *def, nil
 		}
@@ -64,7 +68,7 @@ func (m M) Int(key string, def *int) (v int, err error) {
 func (m M) IntSlice(key string) (v []int, err error) {
 	var val interface{}
 	var ok bool
-	if val, ok = config[key]; !ok {
+	if val, ok = m[key]; !ok {
 		return v, nil
 	}
 	if v, ok = val.([]int); !ok {
@@ -77,7 +81,7 @@ func (m M) IntSlice(key string) (v []int, err error) {
 func (m M) Uint(key string, def *uint) (v uint, err error) {
 	var val interface{}
 	var ok bool
-	if val, ok = config[key]; !ok {
+	if val, ok = m[key]; !ok {
 		if def != nil {
 			return *def, nil
 		}
@@ -92,7 +96,7 @@ func (m M) Uint(key string, def *uint) (v uint, err error) {
 func (m M) UintSlice(key string) (v []uint, err error) {
 	var val interface{}
 	var ok bool
-	if val, ok = config[key]; !ok {
+	if val, ok = m[key]; !ok {
 		return v, nil
 	}
 	if v, ok = val.([]uint); !ok {
@@ -105,7 +109,7 @@ func (m M) UintSlice(key string) (v []uint, err error) {
 func (m M) Int8(key string, def *int8) (v int8, err error) {
 	var val interface{}
 	var ok bool
-	if val, ok = config[key]; !ok {
+	if val, ok = m[key]; !ok {
 		if def != nil {
 			return *def, nil
 		}
@@ -120,7 +124,7 @@ func (m M) Int8(key string, def *int8) (v int8, err error) {
 func (m M) Int8Slice(key string) (v []int8, err error) {
 	var val interface{}
 	var ok bool
-	if val, ok = config[key]; !ok {
+	if val, ok = m[key]; !ok {
 		return v, nil
 	}
 	if v, ok = val.([]int8); !ok {
@@ -133,7 +137,7 @@ func (m M) Int8Slice(key string) (v []int8, err error) {
 func (m M) Uint8(key string, def *uint8) (v uint8, err error) {
 	var val interface{}
 	var ok bool
-	if val, ok = config[key]; !ok {
+	if val, ok = m[key]; !ok {
 		if def != nil {
 			return *def, nil
 		}
@@ -148,7 +152,7 @@ func (m M) Uint8(key string, def *uint8) (v uint8, err error) {
 func (m M) Uint8Slice(key string) (v []uint8, err error) {
 	var val interface{}
 	var ok bool
-	if val, ok = config[key]; !ok {
+	if val, ok = m[key]; !ok {
 		return v, nil
 	}
 	if v, ok = val.([]uint8); !ok {
@@ -161,7 +165,7 @@ func (m M) Uint8Slice(key string) (v []uint8, err error) {
 func (m M) Int16(key string, def *int16) (v int16, err error) {
 	var val interface{}
 	var ok bool
-	if val, ok = config[key]; !ok {
+	if val, ok = m[key]; !ok {
 		if def != nil {
 			return *def, nil
 		}
@@ -176,7 +180,7 @@ func (m M) Int16(key string, def *int16) (v int16, err error) {
 func (m M) Int16Slice(key string) (v []int16, err error) {
 	var val interface{}
 	var ok bool
-	if val, ok = config[key]; !ok {
+	if val, ok = m[key]; !ok {
 		return v, nil
 	}
 	if v, ok = val.([]int16); !ok {
@@ -189,7 +193,7 @@ func (m M) Int16Slice(key string) (v []int16, err error) {
 func (m M) Uint16(key string, def *uint16) (v uint16, err error) {
 	var val interface{}
 	var ok bool
-	if val, ok = config[key]; !ok {
+	if val, ok = m[key]; !ok {
 		if def != nil {
 			return *def, nil
 		}
@@ -204,7 +208,7 @@ func (m M) Uint16(key string, def *uint16) (v uint16, err error) {
 func (m M) Uint16Slice(key string) (v []uint16, err error) {
 	var val interface{}
 	var ok bool
-	if val, ok = config[key]; !ok {
+	if val, ok = m[key]; !ok {
 		return v, nil
 	}
 	if v, ok = val.([]uint16); !ok {
@@ -217,7 +221,7 @@ func (m M) Uint16Slice(key string) (v []uint16, err error) {
 func (m M) Int32(key string, def *int32) (v int32, err error) {
 	var val interface{}
 	var ok bool
-	if val, ok = config[key]; !ok {
+	if val, ok = m[key]; !ok {
 		if def != nil {
 			return *def, nil
 		}
@@ -232,7 +236,7 @@ func (m M) Int32(key string, def *int32) (v int32, err error) {
 func (m M) Int32Slice(key string) (v []int32, err error) {
 	var val interface{}
 	var ok bool
-	if val, ok = config[key]; !ok {
+	if val, ok = m[key]; !ok {
 		return v, nil
 	}
 	if v, ok = val.([]int32); !ok {
@@ -245,7 +249,7 @@ func (m M) Int32Slice(key string) (v []int32, err error) {
 func (m M) Uint32(key string, def *uint32) (v uint32, err error) {
 	var val interface{}
 	var ok bool
-	if val, ok = config[key]; !ok {
+	if val, ok = m[key]; !ok {
 		if def != nil {
 			return *def, nil
 		}
@@ -260,7 +264,7 @@ func (m M) Uint32(key string, def *uint32) (v uint32, err error) {
 func (m M) Uint32Slice(key string) (v []uint32, err error) {
 	var val interface{}
 	var ok bool
-	if val, ok = config[key]; !ok {
+	if val, ok = m[key]; !ok {
 		return v, nil
 	}
 	if v, ok = val.([]uint32); !ok {
@@ -273,7 +277,7 @@ func (m M) Uint32Slice(key string) (v []uint32, err error) {
 func (m M) Int64(key string, def *int64) (v int64, err error) {
 	var val interface{}
 	var ok bool
-	if val, ok = config[key]; !ok {
+	if val, ok = m[key]; !ok {
 		if def != nil {
 			return *def, nil
 		}
@@ -288,7 +292,7 @@ func (m M) Int64(key string, def *int64) (v int64, err error) {
 func (m M) Int64Slice(key string) (v []int64, err error) {
 	var val interface{}
 	var ok bool
-	if val, ok = config[key]; !ok {
+	if val, ok = m[key]; !ok {
 		return v, nil
 	}
 	if v, ok = val.([]int64); !ok {
@@ -301,7 +305,7 @@ func (m M) Int64Slice(key string) (v []int64, err error) {
 func (m M) Uint64(key string, def *uint64) (v uint64, err error) {
 	var val interface{}
 	var ok bool
-	if val, ok = config[key]; !ok {
+	if val, ok = m[key]; !ok {
 		if def != nil {
 			return *def, nil
 		}
@@ -316,7 +320,7 @@ func (m M) Uint64(key string, def *uint64) (v uint64, err error) {
 func (m M) Uint64Slice(key string) (v []uint64, err error) {
 	var val interface{}
 	var ok bool
-	if val, ok = config[key]; !ok {
+	if val, ok = m[key]; !ok {
 		return v, nil
 	}
 	if v, ok = val.([]uint64); !ok {
