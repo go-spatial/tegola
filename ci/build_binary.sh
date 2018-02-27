d396f583c78a0dc35fed133add661261330ba61e
Gautam Dey <gautam@deyfamily.org>
2018-02-26 16:39:13 -0800

    labeled cgo version instead of nocgo. #330

--- ci/build_binary.sh
+++ ci/build_binary.sh
@@ -18,22 +18,22 @@ if [ -z "$TRAVIS_BUILD_DIR" ]; then
 	TRAVIS_BUILD_DIR=.
 fi
 
-echo $TRAVIS_BUILD_DIR
 mkdir -p "${TRAVIS_BUILD_DIR}/releases"
-cd "${TRAVIS_BUILD_DIR}/cmd/tegola"
+echo "building bins into ${TRAVIS_BUILD_DIR}/releases"
+
 for GOARCH in AMD64
 do
 	for GOOS in darwin linux windows
 	do
 		FILENAME="${TRAVIS_BUILD_DIR}/releases/tegola_${GOOS}_${GOARCH}"
-		if [[ $CGO_ENABLED == 0 ]]; then
-			FILENAME="${FILENAME}_nocgo"
+		if [[ $CGO_ENABLED != 0 ]]; then
+			FILENAME="${FILENAME}_cgo"
 		fi
 		if [[ $GOOS == windows ]]; then 
 			FILENAME="${FILENAME}.exe"
 		fi
 
-		go build -ldflags "${LDFLAGS}" -o ${FILENAME}
+		go build -ldflags "${LDFLAGS}" -o ${FILENAME} github.com/terranodo/tegola/cmd/tegola
 		chmod a+x ${FILENAME}
 	done
 done
