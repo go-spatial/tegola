efb6736eb459f1096996c29d34a860eacf13a176
Julio <julio.grillo98@gmail.com>
2018-03-21 22:01:31 -0700

    mend

--- cmd/tegola/cmd/root.go
+++ cmd/tegola/cmd/root.go
@@ -39,6 +39,7 @@ func init() {
 	// cache seed / purge
 	cacheCmd.Flags().StringVarP(&cacheMap, "map", "", "", "map name as defined in the config")
 	cacheCmd.Flags().StringVarP(&cacheZXY, "zxy", "", "", "tile in z/x/y format")
+	cacheCmd.Flags().StringVarP(&cacheFile, "file", "", "", "name of file with zxy values. values must be separated by newlines.")
 	cacheCmd.Flags().UintVarP(&cacheMinZoom, "minzoom", "", 0, "min zoom to seed cache from")
 	cacheCmd.Flags().UintVarP(&cacheMaxZoom, "maxzoom", "", 0, "max zoom to seed cache to")
 	cacheCmd.Flags().StringVarP(&cacheBounds, "bounds", "", "-180,-85.0511,180,85.0511", "lat / long bounds to seed the cache with in the format: minx, miny, maxx, maxy")
