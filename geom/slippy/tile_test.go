aec1dc7ae71c51a14f35f5228ebadb88ca21c688
Julio <julio.grillo98@gmail.com>
2018-03-22 15:03:26 -0700

    reaname RangeChildren -> RangeFamilyAt and added tests for this method

--- geom/slippy/tile_test.go
+++ geom/slippy/tile_test.go
@@ -112,3 +112,97 @@ func TestNewTile(t *testing.T) {
 	}
 
 }
+
+func TestRangeFamilyAt(t *testing.T) {
+	type coord struct {
+		z, x, y uint
+	}
+
+	testcases := map[string]struct {
+		tile     *slippy.Tile
+		zoomAt   uint
+		expected []coord
+	}{
+		"children 1": {
+			tile:   slippy.NewTile(0, 0, 0, 0, tegola.WebMercator),
+			zoomAt: 1,
+			expected: []coord{
+				{1, 0, 0},
+				{1, 0, 1},
+				{1, 1, 0},
+				{1, 1, 1},
+			},
+		},
+		"children 2": {
+			tile:   slippy.NewTile(8, 3, 5, 0, tegola.WebMercator),
+			zoomAt: 10,
+			expected: []coord{
+				{10, 12, 20},
+				{10, 12, 21},
+				{10, 12, 22},
+				{10, 12, 23},
+				//
+				{10, 13, 20},
+				{10, 13, 21},
+				{10, 13, 22},
+				{10, 13, 23},
+				//
+				{10, 14, 20},
+				{10, 14, 21},
+				{10, 14, 22},
+				{10, 14, 23},
+				//
+				{10, 15, 20},
+				{10, 15, 21},
+				{10, 15, 22},
+				{10, 15, 23},
+			},
+		},
+		"parent 1": {
+			tile: slippy.NewTile(1, 0, 0, 0, tegola.WebMercator),
+			zoomAt: 0,
+			expected: []coord{
+				{0, 0, 0},
+			},
+		},
+		"parent 2": {
+			tile: slippy.NewTile(3, 3, 5, 0, tegola.WebMercator),
+			zoomAt: 1,
+			expected: []coord{
+				{1, 0, 1},
+			},
+		},
+	}
+
+	isIn := func(arr []coord, c coord) bool {
+		for _, v := range arr {
+			if v == c {
+				return true
+			}
+		}
+
+		return false
+	}
+
+	for k, tc := range testcases {
+		coordList := make([]coord, 0, len(tc.expected))
+		tc.tile.RangeFamilyAt(tc.zoomAt, func(tile *slippy.Tile) error {
+			z, x, y := tile.ZXY()
+			c := coord{z, x, y}
+
+			coordList = append(coordList, c)
+
+			return nil
+		})
+
+		if len(coordList) != len(tc.expected) {
+			t.Fatalf("[%v] expected coordinate list of length %d, got %d", k, len(tc.expected), len(coordList))
+		}
+
+		for _, v := range tc.expected {
+			if !isIn(coordList, v) {
+				t.Fatalf("[%v] expected coordinate %v missing from list %v", k, v, coordList)
+			}
+		}
+	}
+}
