a78afda6585d89676f6c0bf26ebac428b1d43250
Chris Daley <chebizarro@gmail.com>
2018-03-30 13:53:50 -0700

    removed supressions and crashers folder and added to .gitignore

--- geom/encoding/wkb/gen/main.go
+++ geom/encoding/wkb/gen/main.go
@@ -1,5 +1,4 @@
-// Copyright 2015 Dmitry Vyukov. All rights reserved.
-// Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
+// Generates corpus files from testdata
 
 package main
 
