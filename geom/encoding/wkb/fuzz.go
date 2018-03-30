d0bc9db0b421ff74f4c568642cef86b0ab99a830
Chris Daley <chebizarro@gmail.com>
2018-03-30 11:05:29 -0700

    fuzzes wkb.DecodeBytes

--- geom/encoding/wkb/fuzz.go
+++ geom/encoding/wkb/fuzz.go
@@ -8,16 +8,9 @@ func Fuzz(data []byte) int {
 		if geom != nil {
 			panic("geom != nil on error")
 		}
-		//return 0
-	}
-
-	if bs, err := EncodeBytes(data); err != nil {
-		if bs != nil {
-			panic("bs != nil on error")
-		}
 		return 0
 	}
-	
+
 	return 1
 
 }
