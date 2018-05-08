0f35e2a0b6c8f88988b3eb5f94103c37da15a90c
Julio <julio.grillo98@gmail.com>
2018-05-08 12:38:47 -0700

    new Dicter interface for map abstractions and environment variable implementation of Dicter

---
+++ internal/dict/env/types.go
@@ -0,0 +1,150 @@
+package env
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+)
+
+// matches a variable surrounded by curly braces with leading dollar sign.
+// ex: ${MY_VAR}
+var regex = regexp.MustCompile(`\${[A-Z]+[A-Z1-9_]*}`)
+
+// ErrEnvVar corresponds with a missing environment variable
+type ErrEnvVar struct {
+	varName string
+}
+
+func (ee *ErrEnvVar) Error() string {
+	return fmt.Sprintf("environment variable %q not found", ee.varName)
+}
+
+// ErrType corresponds with an incorrect type passed to UnmarshalTOML
+type ErrType struct {
+	v interface{}
+}
+
+func (te *ErrType) Error() string {
+	return fmt.Sprintf("type %t could not be converted", te.v)
+}
+
+// replaceEnvVars replaces environment variable placeholders in reader stream with values
+func replaceEnvVar(in string) (string, error) {
+	// loop through all environment variable matches
+	for locs := regex.FindStringIndex(in); locs != nil; locs = regex.FindStringIndex(in) {
+
+		// extract match from the input string
+		match := in[locs[0]:locs[1]]
+
+		// trim the leading '${' and trailing '}'
+		varName := match[2 : len(match)-1]
+
+		// get env var
+		envVar, ok := os.LookupEnv(varName)
+		if !ok {
+			return "", &ErrEnvVar{varName: varName}
+		}
+
+		// update the input string with the env values
+		in = strings.Replace(in, match, envVar, -1)
+	}
+
+	return in, nil
+}
+
+//TODO(@ear7h): implement UnmarshalJSON for types
+
+type Bool bool
+
+func BoolPtr(v Bool) *Bool {
+	return &v
+}
+
+func (t *Bool) UnmarshalTOML(v interface{}) error {
+	var b *bool
+	var err error
+
+	b, err = ParseBool(v)
+	if err != nil {
+		return err
+	}
+
+	*t = Bool(*b)
+	return nil
+}
+
+type String string
+
+func StringPtr(v String) *String {
+	return &v
+}
+
+func (t *String) UnmarshalTOML(v interface{}) error {
+	var s *string
+	var err error
+
+	s, err = ParseString(v)
+	if err != nil {
+		return err
+	}
+
+	*t = String(*s)
+	return nil
+}
+
+type Int int
+
+func IntPtr(v Int) *Int {
+	return &v
+}
+
+func (t *Int) UnmarshalTOML(v interface{}) error {
+	var i *int
+	var err error
+
+	i, err = ParseInt(v)
+	if err != nil {
+		return err
+	}
+
+	*t = Int(*i)
+	return nil
+}
+
+type Uint uint
+
+func UintPtr(v Uint) *Uint {
+	return &v
+}
+
+func (t *Uint) UnmarshalTOML(v interface{}) error {
+	var ui *uint
+	var err error
+
+	ui, err = ParseUint(v)
+	if err != nil {
+		return err
+	}
+
+	*t = Uint(*ui)
+	return nil
+}
+
+type Float float64
+
+func FloatPtr(v Float) *Float {
+	return &v
+}
+func (t *Float) UnmarshalTOML(v interface{}) error {
+	var f *float64
+	var err error
+
+	f, err = ParseFloat(v)
+	if err != nil {
+		return err
+	}
+
+	*t = Float(*f)
+	return nil
+}
