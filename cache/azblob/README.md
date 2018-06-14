a1c6b93cdc59e619d8c28ce9f37d3339d6030b5d
Julio <julio.grillo98@gmail.com>
2018-06-14 15:01:42 -0700

    implemented azure blob storage cache

---
+++ cache/azblob/README.md
@@ -0,0 +1,33 @@
+# AzBlobCache
+
+azblob cache is an abstraction on top of Azure Blob Storage which implements the tegola cache interface. To use it, add the following minimum config to your tegola config file:
+
+```toml
+[cache]
+type="azblob"
+container_url="https://your-account.blob.core.windows.net/container-name"
+az_account_name="your-account-name"
+az_shared_key="your-shared-key
+```
+
+## Properties
+The azblob config supports the following properties:
+
+- `container_url` (string): [Required] the name of the S3 bucket to use.
+- `basepath` (string): [Optional] a path prefix added to all cache operations inside of the S3 bucket. helpful so a bucket does not need to be dedicated to only this cache.
+- `az_account_name` (string): [Optional] the storage account to use. `az_shared_key` must be specified with this property. If this is not set, Tegola will only read from the cache.
+- `az_shared_key` (string): [Optional] the storage account key to use.
+- `max_zoom` (int): [Optional] the max zoom the cache should cache to. After this zoom, Set() calls will return before doing work.
+
+## Testing
+Testing is designed to work against a live Azure blob storage account. To run the azblob cache tests, the following environment variables need to be set:
+
+```bash
+$ export RUN_AZBLOB_TESTS=yes
+$ export AZ_CONTAINER_URL='https://your-account.blob.core.windows.net/container-name'
+$ export AZ_ACCOUNT_NAME='your-account'
+$ export AZ_SHARED_KEY='your-key'
+```
+
+## Caveats
+Due to the nature of azblob storage, the first 8 bytes of every cached file is a big-endian binary uint denoting the actual length of the tile.
---
+++ cache/azblob/azblob.go
@@ -0,0 +1,308 @@
+package azblob
+
+import (
+	"path/filepath"
+	"bytes"
+	"net/http"
+	"io/ioutil"
+
+	"github.com/Azure/azure-storage-blob-go/2016-05-31/azblob"
+
+	"github.com/go-spatial/tegola/cache"
+	"net/url"
+	"github.com/go-spatial/tegola/util/dict"
+	"github.com/go-spatial/tegola"
+	"context"
+	"encoding/binary"
+	"fmt"
+	"strconv"
+)
+
+const CacheType = "azblob"
+
+const (
+	ConfigKeyBasepath         = "basepath"
+	ConfigKeyMaxZoom          = "max_zoom"
+	ConfigKeyReadOnly         = "read_only"
+	ConfigKeyContainerUrl     = "container_url"
+	ConfigKeyAzureAccountName = "az_account_name"
+	ConfigKeyAzureSharedKey   = "az_shared_key"
+)
+
+const testMsg = "\x41\x74\x6c\x61\x73\x20\x54\x65\x6c\x61\x6d\x6f\x6e"
+
+func init() {
+	cache.Register(CacheType, New)
+}
+
+func New(config map[string]interface{}) (cache.Interface, error) {
+	azCache := Cache{}
+
+	//parse the config
+	c := dict.M(config)
+
+	// the config map's underlying value is int
+	defaultMaxZoom := uint(tegola.MaxZ)
+	maxZoom, err := c.Uint(ConfigKeyMaxZoom, &defaultMaxZoom)
+	if err != nil {
+		return nil, err
+	}
+
+	azCache.MaxZoom = maxZoom
+
+	// basepath
+	basePath := ""
+	basePath, err = c.String(ConfigKeyBasepath, &basePath)
+	if err != nil {
+		return nil, err
+	}
+
+	azCache.Basepath = basePath
+
+	// TODO: use (dict).Bool method
+	readOnlyStr := strconv.FormatBool(false)
+	readOnlyStr, err = c.String(ConfigKeyReadOnly, &readOnlyStr)
+	if err != nil {
+		return nil, err
+	}
+	readOnly, err := strconv.ParseBool(readOnlyStr)
+	if err != nil {
+		return nil, err
+	}
+
+	azCache.ReadOnly = readOnly
+
+	// credentials
+	acctName := ""
+	acctName, err = c.String(ConfigKeyAzureAccountName, &acctName)
+	if err != nil {
+		return nil, err
+	}
+
+	acctKey := ""
+	acctKey, err = c.String(ConfigKeyAzureSharedKey, &acctKey)
+	if err != nil {
+
+	}
+
+	var cred azblob.Credential
+
+	if acctName+acctKey == "" {
+		cred = azblob.NewAnonymousCredential()
+		azCache.ReadOnly = true
+	} else {
+		if acctName == "" || acctKey == "" {
+			return nil, fmt.Errorf("both %s and %s must be present", ConfigKeyAzureAccountName, ConfigKeyAzureSharedKey)
+		}
+
+		cred = azblob.NewSharedKeyCredential(acctName, acctKey)
+	}
+
+	pipelineOpts := azblob.PipelineOptions{
+		Telemetry: azblob.TelemetryOptions{
+			Value: "tegola-cache",
+		},
+	}
+
+	pipeline := azblob.NewPipeline(cred, pipelineOpts)
+
+	// container
+	uStr, err := c.String(ConfigKeyContainerUrl, nil)
+	if err != nil {
+		return nil, err
+	}
+	u, err := url.Parse(uStr)
+	if err != nil {
+		return nil, err
+	}
+
+	container := azblob.NewContainerURL(*u, pipeline)
+
+	azCache.Container = container
+
+	// in order to confirm we have the correct permissions on the container create a small file
+	// and test a PUT, GET and DELETE to the container
+	key := cache.Key{
+		MapName:   "tegola-test-map",
+		LayerName: "test-layer",
+		Z:         0,
+		X:         0,
+		Y:         0,
+	}
+
+	// seperate test for read only caches
+	if azCache.ReadOnly {
+		// read test file
+		_, _, err := azCache.Get(&key)
+		if err != nil {
+			e := cache.ErrGettingFromCache{
+				Err:       err,
+				CacheType: CacheType,
+			}
+
+			return nil, e
+		}
+	} else {
+		// write test file
+		err = azCache.Set(&key, []byte(testMsg))
+		if err != nil {
+			e := cache.ErrSettingToCache{
+				Err:       err,
+				CacheType: CacheType,
+			}
+
+			return nil, e
+		}
+
+		// read test file
+		byt, hit, err := azCache.Get(&key)
+		if err != nil {
+			e := cache.ErrGettingFromCache{
+				Err:       err,
+				CacheType: CacheType,
+			}
+
+			return nil, e
+		}
+		if !hit {
+			//return an error?
+			panic("no hit on writable cache")
+		}
+
+		// test response of writable cache
+		if string(byt) != testMsg {
+			e := cache.ErrGettingFromCache{
+				Err:       fmt.Errorf("incrrect test response %s != %s", string(byt), testMsg),
+				CacheType: CacheType,
+			}
+
+			return nil, e
+		}
+
+		err = azCache.Purge(&key)
+		if err != nil {
+			e := cache.ErrPurgingCache{
+				Err:       err,
+				CacheType: CacheType,
+			}
+
+			return nil, e
+		}
+	}
+
+	return &azCache, nil
+}
+
+type Cache struct {
+	Basepath  string
+	MaxZoom   uint
+	ReadOnly  bool
+	Container azblob.ContainerURL
+}
+
+func roundUp512(n int) int32 {
+	return int32(n + (512 - n%512))
+}
+
+func (azb *Cache) Set(key *cache.Key, val []byte) error {
+	if key.Z > azb.MaxZoom || azb.ReadOnly {
+		return nil
+	}
+
+	blob := azb.makeBlob(key).ToPageBlobURL()
+	ctx := context.Background()
+
+	httpHeaders := azblob.BlobHTTPHeaders{
+		ContentType: "application/x-protobuf",
+	}
+
+	// must send things in multiples of 512 byte pages
+	msgLen := len(val)
+	blobLen := roundUp512(msgLen + 8)
+
+	// allocate blob
+	blobSlice := make([]byte, blobLen)
+	// encode the length of the blob
+	binary.BigEndian.PutUint64(blobSlice[:8], uint64(msgLen))
+	copy(blobSlice[8:], val)
+
+	_, err := blob.Create(ctx, int64(blobLen), 0, httpHeaders, azblob.Metadata{}, azblob.BlobAccessConditions{})
+	if err != nil {
+		println("\n\n ERR CREATE \n\n")
+		return err
+	}
+
+	pageRange := azblob.PageRange{
+		Start: 0,
+		End:   blobLen - 1,
+	}
+
+	_, err = blob.PutPages(ctx, pageRange, bytes.NewReader(blobSlice), azblob.BlobAccessConditions{})
+	if err != nil {
+		println("\n\n ERR PUT \n\n")
+		return err
+	}
+
+	return nil
+}
+
+func (azb *Cache) Get(key *cache.Key) ([]byte, bool, error) {
+	if key.Z > azb.MaxZoom {
+		return nil, false, nil
+	}
+
+	blob := azb.makeBlob(key)
+	ctx := context.Background()
+
+	res, err := blob.GetBlob(ctx, azblob.BlobRange{}, azblob.BlobAccessConditions{}, false)
+	if err != nil {
+		// check if 404
+		resErr, ok := err.(azblob.ResponseError)
+		if ok {
+			if resErr.Response().StatusCode == http.StatusNotFound {
+				return nil, false, nil
+			}
+		}
+
+		return nil, false, err
+	}
+	defer res.Body().Close()
+
+	blobSlice, err := ioutil.ReadAll(res.Body())
+	if err != nil {
+		return nil, false, err
+	}
+
+	// get the encoded message length
+	msgLen := binary.BigEndian.Uint64(blobSlice[:8])
+
+	// check for out of bounds
+	if msgLen > uint64(len(blobSlice)) {
+		return nil, false, fmt.Errorf("azblob: length section does not match message length")
+	}
+
+	return blobSlice[8:][:msgLen], true, nil
+}
+
+func (azb *Cache) Purge(key *cache.Key) error {
+	if azb.ReadOnly {
+		return nil
+	}
+
+	blob := azb.makeBlob(key)
+	ctx := context.Background()
+
+	_, err := blob.Delete(ctx, azblob.DeleteSnapshotsOptionNone,
+		azblob.BlobAccessConditions{})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (azb *Cache) makeBlob(key *cache.Key) azblob.BlobURL {
+	k := filepath.Join(azb.Basepath, key.String())
+
+	return azb.Container.NewBlobURL(k)
+}
