7359abceb46461d087b8b78be85169dcf7e1814d
Alexander Rolek <a.rolek@gmail.com>
2018-09-04 15:33:52 -0700

    Issue 468 (#503)
    * provider/postgis: replace Mapnik pixel_width/height and scale_denominator SQL tokens
    * added uppercaseTokens func to support case insensitive tokens within ! bangs. mapnik uses lowercase tokens (i.e. !pixel_width!) and tegola uses upper case tokens (i.e. !PIXEL_WIDTH!). this allows for either to work.

--- provider/postgis/util.go
+++ provider/postgis/util.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"strconv"
 	"strings"
+	"unicode"
 
 	"github.com/go-spatial/tegola/basic"
 	"github.com/go-spatial/tegola/provider"
@@ -67,14 +68,20 @@ func genSQL(l *Layer, pool *pgx.ConnPool, tblname string, flds []string) (sql st
 }
 
 const (
-	bboxToken = "!BBOX!"
-	zoomToken = "!ZOOM!"
+	bboxToken             = "!BBOX!"
+	zoomToken             = "!ZOOM!"
+	scaleDenominatorToken = "!SCALE_DENOMINATOR!"
+	pixelWidthToken       = "!PIXEL_WIDTH!"
+	pixelHeightToken      = "!PIXEL_HEIGHT!"
 )
 
 // replaceTokens replaces tokens in the provided SQL string
 //
 // !BBOX! - the bounding box of the tile
 // !ZOOM! - the tile Z value
+// !SCALE_DENOMINATOR! - scale denominator, assuming 90.7 DPI (i.e. 0.28mm pixel size)
+// !PIXEL_WIDTH!       - the pixel width in m, assuming 256x256 tiles
+// !PIXEL_HEIGHT!      - the pixel height in m, assuming 256x256 tiles
 func replaceTokens(sql string, srid uint64, tile provider.Tile) (string, error) {
 
 	bufferedExtent, _ := tile.BufferedExtent()
@@ -95,14 +102,51 @@ func replaceTokens(sql string, srid uint64, tile provider.Tile) (string, error) 
 
 	bbox := fmt.Sprintf("ST_MakeEnvelope(%g,%g,%g,%g,%d)", minPt.X(), minPt.Y(), maxPt.X(), maxPt.Y(), srid)
 
+	extent, _ := tile.Extent()
+	// TODO: Always convert to meter if we support different projections
+	pixelWidth := (extent.MaxX() - extent.MinX()) / 256
+	pixelHeight := (extent.MaxY() - extent.MinY()) / 256
+	scaleDenominator := pixelWidth / 0.00028 /* px size in m */
+
 	// replace query string tokens
 	z, _, _ := tile.ZXY()
 	tokenReplacer := strings.NewReplacer(
 		bboxToken, bbox,
 		zoomToken, strconv.FormatUint(uint64(z), 10),
+		scaleDenominatorToken, strconv.FormatFloat(scaleDenominator, 'f', -1, 64),
+		pixelWidthToken, strconv.FormatFloat(pixelWidth, 'f', -1, 64),
+		pixelHeightToken, strconv.FormatFloat(pixelHeight, 'f', -1, 64),
 	)
 
-	return tokenReplacer.Replace(sql), nil
+	uppercaseTokenSQL, err := uppercaseTokens(sql)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenReplacer.Replace(uppercaseTokenSQL), nil
+}
+
+//	uppercaseTokens will sniff for ! chars and uppercase everything between them
+//	if an odd number of ! are found an error is thrown
+func uppercaseTokens(str string) (string, error) {
+	rs := []rune(str)
+
+	uppercase := false
+	for i := range rs {
+		if rs[i] == '!' {
+			uppercase = !uppercase
+			continue
+		}
+		if uppercase {
+			rs[i] = unicode.ToUpper(rs[i])
+		}
+	}
+
+	if uppercase {
+		return str, ErrUnclosedToken(str)
+	}
+
+	return string(rs), nil
 }
 
 func transformVal(valType pgtype.OID, val interface{}) (interface{}, error) {
