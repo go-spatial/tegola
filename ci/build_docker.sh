f065ed7e9a244732937ca0fc45ab44c9ad28625b
Gautam Dey <gautam.dey77@gmail.com>
2018-10-24 11:32:56 -0700

    [#535] Removed redundant Dockerfile
    We had a redundant Dockerfile which would have to maintained as well
    as the Dockerfile template that is used by our CI system. Removed
    Dockerfile, extrated out the Dockerfile generation and added a README
    to enable one to just generate the Dockerfile.

--- ci/build_docker.sh
+++ ci/build_docker.sh
@@ -6,19 +6,20 @@ if [ "${CGO_ENABLED}" == "0" ]; then
 	exit 0
 fi
 
-
 OLDDIR=$(pwd)
 CI_DIR=`dirname $0`
-PROJECT_DIR="$CI_DIR/.."
-source $CI_DIR/install_go_bin.sh
+PROJECT_DIR="${CI_DIR}/.."
 
-# a cat like program for filling in template vars
-go_install github.com/gdey/bastet
+${PROJECT_DIR}/docker/generate_dockerfile.sh
 
-VERSION_TAG=$TRAVIS_TAG
-if [ -z "$VERSION_TAG" ]; then 
-	VERSION_TAG=$(git rev-parse --short HEAD)
+if [ -z "${VERSION_TAG}" ]; then 
+	 VERSION_TAG=${TRAVIS_TAG}
+	 if [ -z "${VERSION_TAG}" ]; then
+		  VERSION_TAG=$(git rev-parse --short HEAD)
+	 fi
 fi
+
+
 
 PUSH_TO_DOCKER="yes"
 DOCKER_NAME="${DOCKERHUB_ORG}/${DOCKERHUB_REPO}"
@@ -28,20 +29,14 @@ if [ "${DOCKER_NAME}" == "/" ] ; then
 fi
 
 
-LDFLAGS="-w -X github.com/go-spatial/tegola/cmd/tegola/cmd.Version=${VERSION_TAG}"
-CONTAINER_MAINTAINER="a.rolek@gmail.com"
-
-# update our docker template with the provided variables
-bastet -o docker/Dockerfile1 docker/Dockerfile.tpl "flags=-ldflags \"${LDFLAGS}\"" "version=${VERSION_TAG}" "maintainer=${CONTAINER_MAINTAINER}"
-
 # build the container
-docker build -f docker/Dockerfile1 -t ${DOCKER_NAME}:${VERSION_TAG} .
+docker build -f ${PROJECT_DIR}/docker/Dockerfile -t "${DOCKER_NAME}:${VERSION_TAG}" .
 
 # push to docker hub
 if [ "${PUSH_TO_DOCKER}" == "yes" ] ; then
-   docker tag $DOCKERHUB_ORG/$DOCKERHUB_REPO:${VERSION_TAG} $DOCKERHUB_ORG/$DOCKERHUB_REPO:latest
-   docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
-   docker push $DOCKERHUB_ORG/$DOCKERHUB_REPO:${VERSION_TAG}
-   docker push $DOCKERHUB_ORG/$DOCKERHUB_REPO:latest
+   docker tag ${DOCKERHUB_ORG}/${DOCKERHUB_REPO}:${VERSION_TAG} ${DOCKERHUB_ORG}/${DOCKERHUB_REPO}:latest
+   docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}
+   docker push ${DOCKERHUB_ORG}/${DOCKERHUB_REPO}:${VERSION_TAG}
+   docker push ${DOCKERHUB_ORG}/${DOCKERHUB_REPO}:latest
 fi
 
