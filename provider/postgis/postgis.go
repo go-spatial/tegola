ae8f5307e0de1c316565b484be0b77eb3adc7e4e
Alexander Rolek <a.rolek@gmail.com>
2019-04-04 21:59:58 -0700

    handle empty geometry collection

--- provider/postgis/postgis.go
+++ provider/postgis/postgis.go
@@ -581,6 +581,12 @@ func (p Provider) TileFeatures(ctx context.Context, layer string, tile provider.
 			continue
 		}
 
+		// skip row if geometry collection empty.
+		g, ok := geometry.(geom.Collection)
+		if ok && len(g.Geometries()) == 0 {
+			continue
+		}
+
 		// decode our WKB
 		geom, err := wkb.DecodeBytes(geobytes)
 		if err != nil {
