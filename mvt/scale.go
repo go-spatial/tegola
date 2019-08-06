debb5cc3f3d470d28921c033adf325c7c9d85748
Julio <julio.grillo98@gmail.com>
2019-08-05 20:58:33 -0700

    remove basic types from mvt package (#629)
    * remove basic types from mvt package
    * move internal logger out of mvt package for cutover of mvt into the geom package

--- mvt/scale.go
+++ mvt/scale.go
@@ -1,28 +1,33 @@
 package mvt
 
 import (
+	"log"
+
+	"github.com/go-spatial/geom"
+	"github.com/go-spatial/geom/cmp"
 	"github.com/go-spatial/tegola"
-	"github.com/go-spatial/tegola/basic"
-	"github.com/go-spatial/tegola/internal/log"
 )
 
 // ScaleGeo converts the geometry's coordinates to tile coordinates
-func ScaleGeo(geo tegola.Geometry, tile *tegola.Tile) basic.Geometry {
+func ScaleGeo(geo tegola.Geometry, tile *tegola.Tile) geom.Geometry {
 	switch g := geo.(type) {
-	case tegola.Point:
+	case geom.Point:
 		return scalept(g, tile)
 
-	case tegola.Point3:
-		return scalept(g, tile)
-
-	case tegola.MultiPoint:
+	case geom.MultiPoint:
 		pts := g.Points()
 		if len(pts) == 0 {
 			return nil
 		}
-		var ptmap = make(map[basic.Point]struct{})
-		var mp = make(basic.MultiPoint, 0, len(pts))
-		mp = append(mp, scalept(pts[0], tile))
+
+		// TODO the ptmap is used for removing duplicate points.
+		// This does not seem to be part of the spec, but it seems
+		// helpful for reducing the point count, especially with
+		// very zoomed out geometries. Further discussion should be had...
+		// https://github.com/mapbox/vector-tile-spec/tree/master/2.1#4342-point-geometry-type
+		var ptmap = make(map[geom.Point]struct{})
+		var mp = make(geom.MultiPoint, 1, len(pts))
+		mp[0] = scalept(pts[0], tile)
 
 		ptmap[mp[0]] = struct{}{}
 		for i := 1; i < len(pts); i++ {
@@ -38,12 +43,12 @@ func ScaleGeo(geo tegola.Geometry, tile *tegola.Tile) basic.Geometry {
 		}
 		return mp
 
-	case tegola.LineString:
+	case geom.LineString:
 		return scalelinestr(g, tile)
 
-	case tegola.MultiLine:
-		var ml basic.MultiLine
-		for _, l := range g.Lines() {
+	case geom.MultiLineString:
+		var ml geom.MultiLineString
+		for _, l := range g.LineStrings() {
 			nl := scalelinestr(l, tile)
 			if len(nl) > 0 {
 				ml = append(ml, nl)
@@ -51,11 +56,11 @@ func ScaleGeo(geo tegola.Geometry, tile *tegola.Tile) basic.Geometry {
 		}
 		return ml
 
-	case tegola.Polygon:
+	case geom.Polygon:
 		return scalePolygon(g, tile)
 
-	case tegola.MultiPolygon:
-		var mp basic.MultiPolygon
+	case geom.MultiPolygon:
+		var mp geom.MultiPolygon
 		for _, p := range g.Polygons() {
 			np := scalePolygon(p, tile)
 			if len(np) > 0 {
@@ -65,30 +70,30 @@ func ScaleGeo(geo tegola.Geometry, tile *tegola.Tile) basic.Geometry {
 		return mp
 	}
 
-	return basic.G{}
+	return nil
 }
 
-func scalept(g tegola.Point, tile *tegola.Tile) basic.Point {
-	pt, err := tile.ToPixel(tegola.WebMercator, [2]float64{g.X(), g.Y()})
+func scalept(g geom.Point, tile *tegola.Tile) geom.Point {
+	pt, err := tile.ToPixel(tegola.WebMercator, g)
 	if err != nil {
 		panic(err)
 	}
-	return basic.Point{pt[0], pt[1]}
+	return geom.Point(pt)
 }
 
-func scalelinestr(g tegola.LineString, tile *tegola.Tile) (ls basic.Line) {
-	pts := g.Subpoints()
+func scalelinestr(g geom.LineString, tile *tegola.Tile) (ls geom.LineString) {
+	pts := g
 	// If the linestring
 	if len(pts) < 2 {
 		// Not enought points to make a line.
 		return nil
 	}
-	ls = make(basic.Line, 0, len(pts))
+	ls = make(geom.LineString, 0, len(pts))
 	ls = append(ls, scalept(pts[0], tile))
 	lidx := len(ls) - 1
 	for i := 1; i < len(pts); i++ {
 		npt := scalept(pts[i], tile)
-		if tegola.IsPointEqual(ls[lidx], npt) {
+		if cmp.PointEqual(ls[lidx], npt) {
 			// drop any duplicate points.
 			continue
 		}
@@ -103,19 +108,20 @@ func scalelinestr(g tegola.LineString, tile *tegola.Tile) (ls basic.Line) {
 	return ls
 }
 
-func scalePolygon(g tegola.Polygon, tile *tegola.Tile) (p basic.Polygon) {
-	lines := g.Sublines()
-	p = make(basic.Polygon, 0, len(lines))
+func scalePolygon(g geom.Polygon, tile *tegola.Tile) (p geom.Polygon) {
+	lines := geom.MultiLineString(g.LinearRings())
+	p = make(geom.Polygon, 0, len(lines))
 
 	if len(lines) == 0 {
 		return p
 	}
-	for i := range lines {
-		ln := scalelinestr(lines[i], tile)
+
+	for _, line := range lines.LineStrings() {
+		ln := scalelinestr(line, tile)
 		if len(ln) < 2 {
 			if debug {
 				// skip lines that have been reduced to less then 2 points.
-				log.Debug("skipping line 2", lines[i], len(ln))
+				log.Println("skipping line 2", line, len(ln))
 			}
 			continue
 		}
