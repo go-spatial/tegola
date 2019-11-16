200b6e8bea7742b7e58f329b0ca0d7023c5a867e
Antoine GIRARD <sapk@sapk.fr>
2019-11-16 21:15:35 +0100

    feat(cache/mbtiles): add invalid bound case

--- internal/cmd/latlng.go
+++ internal/cmd/latlng.go
@@ -9,12 +9,12 @@ func IsValidLat(f64 float64) bool { return -90.0 <= f64 && f64 <= 90.0 }
 
 func IsValidLatString(lat string) (float64, bool) {
 	f64, err := strconv.ParseFloat(strings.TrimSpace(lat), 64)
-	return f64, err == nil || IsValidLat(f64)
+	return f64, err == nil && IsValidLat(f64)
 }
 
 func IsValidLng(f64 float64) bool { return -180.0 <= f64 && f64 <= 180.0 }
 
 func IsValidLngString(lng string) (float64, bool) {
 	f64, err := strconv.ParseFloat(strings.TrimSpace(lng), 64)
-	return f64, err == nil || IsValidLng(f64)
+	return f64, err == nil && IsValidLng(f64)
 }
