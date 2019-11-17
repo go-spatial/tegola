a89d41ffcbef17c72f07e175be3d2e7262357f99
Antoine GIRARD <sapk@sapk.fr>
2019-11-17 01:10:58 +0100

    fix: typo

--- atlas/cache_azblob.go
+++ atlas/cache_azblob.go
@@ -4,7 +4,7 @@ package atlas
 
 // The point of this file is to load and register the azblob cache backend.
 // the azblob cache can be excluded during the build with the `noAzblobCache` build flag
-// for example from the cmd/tegola direcotry:
+// for example from the cmd/tegola directory:
 //
 // go build -tags 'noAzblobCache'
 import (
--- atlas/cache_redis.go
+++ atlas/cache_redis.go
@@ -4,7 +4,7 @@ package atlas
 
 // The point of this file is to load and register the redis cache backend.
 // the redis cache can be excluded during the build with the `noRedisCache` build flag
-// for example from the cmd/tegola direcotry:
+// for example from the cmd/tegola directory:
 //
 // go build -tags 'noRedisCache'
 import (
