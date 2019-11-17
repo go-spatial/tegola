a407db9c7764f3be9fbb066b842ef91d885c048a
Antoine GIRARD <sapk@sapk.fr>
2019-11-17 01:10:58 +0100

    fix: typo

--- atlas/cache_s3.go
+++ atlas/cache_s3.go
@@ -4,7 +4,7 @@ package atlas
 
 // The point of this file is to load and register the s3 cache backend.
 // the s3 cache can be excluded during the build with the `noS3Cache` build flag
-// for example from the cmd/tegola direcotry:
+// for example from the cmd/tegola directory:
 //
 // go build -tags 'noS3Cache'
 import (
--- atlas/provider_gpkg.go
+++ atlas/provider_gpkg.go
@@ -4,7 +4,7 @@ package atlas
 
 // The point of this file is to load and register the GeoPackage provider.
 // the GeoPackage provider can be excluded during the build with the `noGpkgProvider` build flag
-// for example from the cmd/tegola direcotry:
+// for example from the cmd/tegola directory:
 //
 // go build -tags 'noGpkgProvider'
 import (
