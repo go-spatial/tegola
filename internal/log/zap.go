ac60a09d32031dbbe2632cfae9864a308547b00c
Benjamin Ramser <legionaerr@googlemail.com>
2022-05-07 15:41:09 +0200

    feat: structured logs with uber/zap
    As discussed in #831 I added the option to use structured logs with uber/zap
    while falling back to the currently implemented default if --logger
    is not specifically used. Log levels conform to the current implementation
    with the exception of log level TRACE that is not supported by zap.
    I use Debug as a fall back here.

---
+++ internal/log/zap.go
@@ -0,0 +1,62 @@
+package log
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var zapLogger *ZapLogger
+
+type ZapLogger struct {
+	*zap.SugaredLogger
+}
+
+// Trace ...
+// Trace is not a supported log level by uber/zap
+// To satisfy log.Interface
+func (z *ZapLogger) Trace(args ...interface{}) {
+	z.Debug(args)
+}
+
+func newZapDefaultConfig() zap.Config {
+	return zap.Config{
+		Level:       zap.NewAtomicLevelAt(zap.DebugLevel),
+		Development: false,
+		Sampling: &zap.SamplingConfig{
+			Initial:    100,
+			Thereafter: 100,
+		},
+		Encoding: "json",
+		EncoderConfig: zapcore.EncoderConfig{
+			TimeKey:        "timestamp",
+			LevelKey:       "level",
+			NameKey:        "logger",
+			CallerKey:      "caller",
+			MessageKey:     "message",
+			StacktraceKey:  "stacktrace",
+			LineEnding:     zapcore.DefaultLineEnding,
+			EncodeLevel:    zapcore.LowercaseLevelEncoder,
+			EncodeTime:     zapcore.ISO8601TimeEncoder,
+			EncodeDuration: zapcore.SecondsDurationEncoder,
+			EncodeCaller:   zapcore.ShortCallerEncoder,
+		},
+		OutputPaths:      []string{"stdout"},
+		ErrorOutputPaths: []string{"stderr"},
+	}
+}
+
+func buildZapLogger() error {
+	zapconfig := newZapDefaultConfig()
+
+	zapconfig.DisableCaller = true
+	core, err := zapconfig.Build()
+	if err != nil {
+		return err
+	}
+
+	defer core.Sync()
+	sl := core.Sugar()
+
+	zapLogger = &ZapLogger{sl}
+	return nil
+}
